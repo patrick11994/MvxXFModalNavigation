cf6ed445954a71140b8179b4220c7e91b383e3c5
Patrick de Cubber <patrick.de.cubber@trust.com>
2020-08-29 17:01:36 +0200

    Added [MvxModalPresentation] on screen A and B

--- MvxXFModalNavigation/MvxXFModalNavigation/Views/ScreenDView.xaml.cs
+++ MvxXFModalNavigation/MvxXFModalNavigation/Views/ScreenDView.xaml.cs
@@ -1,9 +1,11 @@
-﻿using MvvmCross.Forms.Views;
+﻿using MvvmCross.Forms.Presenters.Attributes;
+using MvvmCross.Forms.Views;
 using Xamarin.Forms.Xaml;
 
 namespace MvxXFModalNavigation.Views
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
+    [MvxModalPresentation]
     public partial class ScreenDView : MvxContentPage
     {
         public ScreenDView()
